89272df0a0490fbce7ec13bc002fca6ad4fa6d68
paulc-zededa <paulc@zededa.com>
2025-09-13 16:54:08 +0200

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    zedamigo = {
+      source  = "localhost/andrei-zededa/zedamigo"
+      version = "0.5.0"
+    }
+
+    zedcloud = {
+      source  = "zededa/zedcloud"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "zedamigo" {
+  # target = ""
+  use_sudo = true
+}
+
+variable "ZEDEDA_CLOUD_URL" {
+  description = "ZEDEDA CLOUD URL"
+  sensitive   = false
+  type        = string
+}
+
+variable "ZEDEDA_CLOUD_TOKEN" {
+  description = "ZEDEDA CLOUD API TOKEN"
+  sensitive   = true
+  type        = string
+}
+
+provider "zedcloud" {
+  zedcloud_url   = var.ZEDEDA_CLOUD_URL
+  zedcloud_token = var.ZEDEDA_CLOUD_TOKEN
+}
